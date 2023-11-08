22b02cf2da4e46b1f626bb716cbd8f2987823ade
raflyyunandia <raflyyunandia@gmail.com>
2023-11-08 15:29:44 +0700

    try todolist for free

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="copyright">&copy; 2023 <a href='https://nextjs-raflyyunandia.vercel.app' target='_blank' rel="noreferrer">Rafly</a> All Right Reserved</div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer;
